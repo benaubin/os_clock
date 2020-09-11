8199098e2342eb1623b1f20ea5512def7a0c67b8
Ben Aubin <ben@benaubin.com>
2020-09-10 21:53:17 -0500

    cargo fmt

--- src/timer.rs
+++ src/timer.rs
@@ -7,20 +7,20 @@ use super::Clock;
 ///
 /// Get a timer with `Clock.start_timer()`
 pub struct Timer<'c, C: Clock> {
-  pub start: Duration,
-  pub clock: &'c C,
+    pub start: Duration,
+    pub clock: &'c C,
 }
 
 impl<'c, C: Clock> Timer<'c, C> {
-  /// Returns the ellapsed time since `start`
-  pub fn elapsed(&self) -> Result<Duration> {
-    let end = self.clock.get_time()?;
-    return Ok(end - self.start);
-  }
+    /// Returns the ellapsed time since `start`
+    pub fn elapsed(&self) -> Result<Duration> {
+        let end = self.clock.get_time()?;
+        return Ok(end - self.start);
+    }
 
-  /// Drops the timer and returns the ellapsed time since `start`
-  #[inline(always)]
-  pub fn end(self) -> Result<Duration> {
-    return self.elapsed();
-  }
+    /// Drops the timer and returns the ellapsed time since `start`
+    #[inline(always)]
+    pub fn end(self) -> Result<Duration> {
+        return self.elapsed();
+    }
 }
